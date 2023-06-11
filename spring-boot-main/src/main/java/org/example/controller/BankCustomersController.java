e38bbe07a1b22598551130838e3c13aed9ab2332
Soumik Mukherjee <msoumik@Soumiks-MacBook-Pro.local>
2023-06-11 09:20:38 +0200

    removed unnecessary stuff and just kept the part required to demo dynamic change in log levels

--- spring-boot-main/src/main/java/org/example/controller/BankCustomersController.java
+++ spring-boot-main/src/main/java/org/example/controller/BankCustomersController.java
@@ -1,45 +1,28 @@
 package org.example.controller;
 
-import jakarta.validation.Valid;
-import org.example.BankCustomer;
+import lombok.extern.slf4j.Slf4j;
 import org.example.service.BankCustomersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Validated
 @RestController
 @RequestMapping("/api/1/bank-customers")
+@Slf4j
 public class BankCustomersController {
 
     @Autowired
     BankCustomersService bankCustomersService;
 
     @GetMapping(value = "/{customerName}", produces = {"application/json"})
-    public BankCustomer getCustomerDetails(@PathVariable("customerName") final String bankCustomerName) {
+    public String getCustomerMessage(@PathVariable("customerName") final String bankCustomerName) {
+        log.info("In controller layer, request received for {} ", bankCustomerName);
         return bankCustomersService.getCustomerDetail(bankCustomerName);
     }
 
-    @PostMapping(produces = {"application/json"})
-    public void createCustomer(@Valid @RequestBody BankCustomer bankCustomer) {
-        bankCustomersService.createCustomer(bankCustomer);
-    }
-
-    @DeleteMapping(value = "/{customerName}", produces = {"application/json"})
-    public void deleteCustomer(@PathVariable("customerName") final String bankCustomerName) {
-        bankCustomersService.removeCustomer(bankCustomerName);
-    }
-
-    @PutMapping(value = "/{customerName}", produces = {"application/json"})
-    public void updateCustomer(@PathVariable("customerName") final String bankCustomerName, @RequestBody BankCustomer bankCustomerUpdated) {
-        bankCustomersService.updateCustomer(bankCustomerName, bankCustomerUpdated);
-    }
 
 }
--- spring-boot-main/src/main/java/org/example/service/BankCustomersService.java
+++ spring-boot-main/src/main/java/org/example/service/BankCustomersService.java
@@ -1,38 +1,15 @@
 package org.example.service;
 
-import org.example.BankCustomer;
-import org.example.dao.IDao;
-import org.example.exception.BusinessException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class BankCustomersService {
 
-    @Autowired
-    @Qualifier("SimpleDao")
-    IDao iDao;
-
-    public void createCustomer(BankCustomer bankCustomer) {
-        iDao.createCustomer(bankCustomer);
-    }
-
-    public BankCustomer getCustomerDetail(String bankCustomerName) {
-        BankCustomer bankCustomer=  iDao.getCustomerDetail(bankCustomerName);
-        if (bankCustomer == null) {
-            throw new BusinessException("Customer does not exist");
-        } else {
-            return bankCustomer;
-        }
-    }
-
-    public void removeCustomer(String bankCustomerName) {
-        iDao.removeCustomerDetail(bankCustomerName);
-    }
-
-    public void updateCustomer(String bankCustomerName, BankCustomer bankCustomerUpdated) {
-        iDao.updateCustomerDetail(bankCustomerName, bankCustomerUpdated);
+    public String getCustomerDetail(String bankCustomerName) {
+        log.info("In service layer, request received for {} ", bankCustomerName);
+        return "Hello "+bankCustomerName;
     }
 
 }
